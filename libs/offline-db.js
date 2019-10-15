d3309eba98488264c74d7a578e0cba6bc82fbd18
AllenjinrongZhang <AllenjinrongZhang@gmail.com>
2019-10-15 10:14:17 +0930

    finished adding data in Json

--- libs/offline-db.js
+++ libs/offline-db.js
@@ -10,5 +10,15 @@ const offlineEnglishDB = {
         },
         "desc":"n. 苹果，苹果树，苹果似的东西；[美俚]炸弹，手榴弹，（棒球的）球；[美俚]人，家伙。"
     },
-    
+    "computer":{
+        en:{
+            "voc":"[kəmˈpjuːtə(r)]",
+            "audio":"audios/computer-en.mp3",
+        },
+        us:{
+            "voc":"[kəmˈpjuːtər]",
+            "audio":"audios/computer-us.mp3",
+        },
+        "desc":"n. 计算机；电脑；电子计算机"
+    },
 };
