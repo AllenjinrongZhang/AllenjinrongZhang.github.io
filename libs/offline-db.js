3f1d046afeb86594eecbdb38cf192c8473ecc7ed
AllenjinrongZhang <AllenjinrongZhang@gmail.com>
2019-10-15 10:12:12 +0930

    add some contents in jason file

--- libs/offline-db.js
+++ libs/offline-db.js
@@ -1,5 +1,14 @@
 const offlineEnglishDB = {
-    "apple":"n. 苹果，苹果树，苹果似的东西；[美俚]炸弹，手榴弹，（棒球的）球；[美俚]人，家伙。",
-    "computer":"n. 计算机；电脑；电子计算机",
-    "MVP": "缩写：最小有价值的产品"
+    "apple":{
+        en:{
+            "voc":"[ˈæpl]",
+            "audio":"audios/apple-en.mp3",
+        },
+        us:{
+            "voc":"[ˈæpl]",
+            "audio":"audios/apple-us.mp3",
+        },
+        "desc":"n. 苹果，苹果树，苹果似的东西；[美俚]炸弹，手榴弹，（棒球的）球；[美俚]人，家伙。"
+    },
+    
 };
