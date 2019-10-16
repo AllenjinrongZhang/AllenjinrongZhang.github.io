4563f6564c755c7724da686bea6d49f172b11b75
AllenjinrongZhang <AllenjinrongZhang@gmail.com>
2019-10-17 09:21:51 +0930

    continue to modify offline CHinese translation

--- libs/offline-db.js
+++ libs/offline-db.js
@@ -43,4 +43,17 @@ const offlineChineseDB = {
             "audio":"audios/apple-us.mp3",
         },
         "desc":"a round fruit with firm, white flesh and a green, red, or yellow skin"
-    };
+    },
+    "计算机":{
+        "word":"computer",
+        en:{
+            "voc":"[kəmˈpjuːtə(r)]",
+            "audio":"audios/computer-en.mp3",
+        },
+        us:{
+            "voc":"[kəmˈpjuːtər]",
+            "audio":"audios/computer-us.mp3",
+        },
+        "desc":"an electronic machine that is used for storing, organizing, and finding words, numbers, and pictures, for doing calculations, and for controlling other machines"
+    },
+};
