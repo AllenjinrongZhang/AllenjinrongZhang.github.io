549a43d5a22c2fe65d37bdce67d5b0a72226f388
AllenjinrongZhang <AllenjinrongZhang@gmail.com>
2019-10-17 22:55:14 +0930

    before debugging with encountering interface issure

--- libs/offline-db.js
+++ libs/offline-db.js
@@ -1,3 +1,27 @@
+const offlineEnglishDB = {
+    "apple":{
+        en:{
+            "voc":"[ˈæpl]",
+            "audio":"audios/apple-en.mp3",
+        },
+        us:{
+            "voc":"[ˈæpl]",
+            "audio":"audios/apple-us.mp3",
+        },
+        "desc":"n. 苹果，苹果树，苹果似的东西；[美俚]炸弹，手榴弹，（棒球的）球；[美俚]人，家伙。"
+    },
+    "computer":{
+        en:{
+            "voc":"[kəmˈpjuːtə(r)]",
+            "audio":"audios/computer-en.mp3",
+        },
+        us:{
+            "voc":"[kəmˈpjuːtər]",
+            "audio":"audios/computer-us.mp3",
+        },
+        "desc":"n. 计算机；电脑；电子计算机"
+    },
+};
 
 
 const offlineChineseDB = {
