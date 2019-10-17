2488d79da54eec6b63bc5fd76aa87d066e60cdd7
AllenjinrongZhang <AllenjinrongZhang@gmail.com>
2019-10-17 09:42:47 +0930

    before modify interface1

--- libs/offline-db.js
+++ libs/offline-db.js
@@ -1,35 +1,4 @@
-const offlineEnglishDB = {
-    "apple":{
-        en:{
-            "voc":"[ˈæpl]",
-            "audio":"audios/apple-en.mp3",
-        },
-        us:{
-            "voc":"[ˈæpl]",
-            "audio":"audios/apple-us.mp3",
-        },
-        "desc":"n. 苹果，苹果树，苹果似的东西；[美俚]炸弹，手榴弹，（棒球的）球；[美俚]人，家伙。"
-    },
-    "computer":{
-        en:{
-            "voc":"[kəmˈpjuːtə(r)]",
-            "audio":"audios/computer-en.mp3",
-        },
-        us:{
-            "voc":"[kəmˈpjuːtər]",
-            "audio":"audios/computer-us.mp3",
-        },
-        "desc":"n. 计算机；电脑；电子计算机"
-    },
-    "preschool":{
-        en:{
-            "voc":"[/ˈpriːskuːl/]",
-            "audio":"",
-        },
-        "desc":"n. 幼儿园，育幼院"
-    
-    }
-};
+
 
 const offlineChineseDB = {
     "苹果":{
